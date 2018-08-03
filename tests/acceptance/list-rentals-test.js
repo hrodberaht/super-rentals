15d3a87af0b7cf076cce28e83ced36561568eea0
Robert <seedraev@gmail.com>
2018-08-03 20:06:00 +0200

    add routes and tests for it

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,15 +1,27 @@
 import { module, test } from "qunit";
-import { visit, currentURL } from "@ember/test-helpers";
+import { click, currentURL, visit } from "@ember/test-helpers";
 import { setupApplicationTest } from "ember-qunit";
 
 module("Acceptance | list rentals", function(hooks) {
   setupApplicationTest(hooks);
 
-  test("should show rentals as the home page", async function(assert) {});
+  test("should show rentals as the home page", async function(assert) {
+    await visit("/");
+    assert.equal(currentURL(), "/rentals", "shuld redirect from / to rentals ");
+  });
 
-  test("should link to information about the company.", async function(assert) {});
+  test("should link to information about the company.", async function(assert) {
+    await visit("/");
+    await click(".menu-about");
 
-  test("should link to contact information.", async function(assert) {});
+    assert.equal(currentURL(), "/about", "should navigate to about");
+  });
+
+  test("should link to contact information.", async function(assert) {
+    await visit("/");
+    await click(".menu-contact");
+    assert.equal(currentURL(), "/contact", "should navigate to contact");
+  });
 
   test("should list available rentals.", async function(assert) {});
 
